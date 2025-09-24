559d30256adafc5989cae128b316d19db6c88920
CarlosGM <jgoveamagana@gmail.com>
2025-09-23 18:31:21 -0600

    Avance general del proyecto

---
+++ utils/colors.py
@@ -0,0 +1,14 @@
+# Paleta de colores para MathRoots
+
+# Colores principales
+PRIMARY_COLOR = "#CD1C18"       
+SECONDARY_COLOR = "#9B1313"    
+
+# Fondos
+BACKGROUND_COLOR = "#F3F3F3"   
+CARD_COLOR = "#FFFFFF"           
+
+# Textos
+TEXT_COLOR = "#000000"           
+TEXT_LIGHT = "#9F9F9F"          
+TEXT_INVERT = "#FFFFFF"          
